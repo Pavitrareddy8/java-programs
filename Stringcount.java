78312bb78d596196ba0898dfe1243942e1914543
Pavitrareddy8 <pavitrareddy785@gmail.com>
2025-06-02 16:16:08 +0530

    Add files via upload

---
+++ Stringcount.java
@@ -0,0 +1,17 @@
+ java.util.*;
+public class Stringcou{
+  public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+	 String a=sc.nextLine();
+	 char b=sc.next().charAt(0);
+	 int count=0;
+	 for(int i=0;i<a.length();i++){
+	 char ch=a.charAt(i);
+	 if(ch==b){
+	 count+=1;
+	 }	 
+  }
+  	 System.out.println(count);
+
+}
+}
