796d7573b0043f1730532917974d327bc69cad9d
Pavitrareddy8 <pavitrareddy785@gmail.com>
2025-06-17 14:33:36 +0530

    Add files via upload

---
+++ queue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class queue{
+public static void main(String[] args){
+Queue<Integer> q=new LinkedList<>();
+q.add(1);
+q.add(2);
+System.out.print(q);
+System.out.println(q.remove());
+System.out.print(q);
+
+}
+
+}
